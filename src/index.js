56170e8097796153cfab21a5747f7d13ab8a7ca9
derpmagician <renzin@gmail.com>
2023-08-16 16:16:52 -0500

    adding auth0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { Auth0Provider } from '@auth0/auth0-react';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter,
 } from "react-router-dom";
 import store from "./store/store";
@@ -13,20 +13,20 @@ import "primereact/resources/primereact.min.css";
 import 'primeicons/primeicons.css';
 // import 'primeflex/primeflex.css';
 
-// console.log("ENV", process.env.REACT_APP_AUTH0_DOMAIN);
-// const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-// const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+console.log("ENV", process.env);
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Auth0Provider
+    <Auth0Provider
       domain={domain}
       clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin
       }}
-    > */}
+    >
       <Provider store={store}>
       <BrowserRouter basename={process.env.NODE_ENV === 'production' ? "/user-administration-react-redux" : process.env.BASENAME}>
       {/* <BrowserRouter basename={process.env.NODE_ENV === 'production' ? "/user-administration-react-redux" : "/"}> */}
@@ -34,7 +34,7 @@ root.render(
       </BrowserRouter>
       </Provider>
     
-    {/* </Auth0Provider> */}
+    </Auth0Provider>
   </React.StrictMode>
 );
 
