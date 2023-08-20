53c4fc6c3c269de5957aa29ec25e02cba5bdb31c
derpmagician <renzin@gmail.com>
2023-08-19 20:05:40 -0500

    agregando persistreducer

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter, HashRouter } from "react-router-dom";
-import store from "./store/store";
+// import store from "./store/store";
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from "./store/store"
 import { Provider } from "react-redux";
+
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
@@ -15,7 +18,10 @@ import 'primeicons/primeicons.css';
 // console.log("ENV=>", process.env);
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
-const redirigir =  process.env.NODE_ENV === 'production' ? "https://derpmagician.github.io/user-administration-react-redux/" : window.location.origin
+const redirigir =  process.env.NODE_ENV === 'production' ?
+  "https://derpmagician.github.io/user-administration-react-redux/"
+  :
+  window.location.origin
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,19 +30,21 @@ root.render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        // redirect_uri: window.location.origin
         redirect_uri: redirigir
       }}
     >
       <Provider store={store}>
       {/* <HashRouter  basename={process.env.NODE_ENV === 'production' ? "/user-administration-react-redux" : process.env.BASENAME}> */}
       {/* <BrowserRouter basename={process.env.NODE_ENV === 'production' ? "/user-administration-react-redux" : "/"}> */}
-      <HashRouter>
-        <App />
-      </HashRouter >
+      <PersistGate loading={null} persistor={persistor}>
+        <HashRouter>
+          <App />
+        </HashRouter >
+      </PersistGate>
       </Provider>
-    
     </Auth0Provider>
 
 );
+
+
 
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore, combineReducers } from "@reduxjs/toolkit"
 import {dptosSlice, miSlice} from './miSlice'
+import storage from 'redux-persist/lib/storage'; // importa el almacenamiento local
+import { persistReducer, persistStore } from 'redux-persist';
 
-export default configureStore({
-  reducer: {
-    misTrabajadores: miSlice.reducer,
-    misDptos: dptosSlice.reducer,
+const persistConfig = {
+  key: 'root',
+  storage,
+}
 
-  }
+// export default configureStore({
+//   reducer: {
+//     misTrabajadores: miSlice.reducer,
+//     misDptos: dptosSlice.reducer,
+
+//   }
+// })
+
+const rootReducer = combineReducers({
+  misTrabajadores: miSlice.reducer,
+  misDptos: dptosSlice.reducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer) // crea un reducer persistido
+
+const store = configureStore({
+  reducer: persistedReducer, // usa el reducer persistido en lugar de rootReducer
+})
+
+export const persistor = persistStore(store) // crea un persistor con el store
+
+export default store;
+
+
